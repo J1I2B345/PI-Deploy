39f0480952552315e1239b4f82d3952a256513de
J1I2B345 <juan.bevilacqua@outlook.com>
2022-07-20 16:15:24 -0300

    defaultvalue a img

--- api/src/models/Pokemon.js
+++ api/src/models/Pokemon.js
@@ -34,7 +34,8 @@ module.exports = (sequelize) => {
       type: DataTypes.INTEGER
     },
     img: {
-      type: DataTypes.STRING
+      type: DataTypes.STRING,
+      defaultValue: 'https://thumbs.gfycat.com/FairRectangularBlacklemur.webp'
     }
   },
   {
