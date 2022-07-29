dac8d3d5844937eb4f8fdec660887dcc606ccf58
J1I2B345 <juan.bevilacqua@outlook.com>
2022-07-29 17:17:16 -0300

    return to the link

--- client/src/components/Pokemon.js
+++ client/src/components/Pokemon.js
@@ -6,23 +6,21 @@ const Pokemon = (props)=>{
     
     return (
         <div className={`${s.div} ${s[`number${props.i}`]} `}>
-            <Link  to={`/pokemon/Id/${props.id}`} className={s.link}>
-                <div className={s.divname}> 
-                    <h1 className={s.h1}> {props.name.toUpperCase()}</h1>           
+            <div className={s.divname}> 
+                    <Link  to={`/pokemon/Id/${props.id}`} className={s.link}> <h1 className={s.h1}> {props.name.toUpperCase()}</h1> </Link>            
                 </div>
-                <div className={s.divimg}> 
-                    <img className={s.img} src={props.img} alt={`Imagen de pokemon ${props.name}`}/>
+            <div className={s.divimg}> 
+                <img className={s.img} src={props.img} alt={`Imagen de pokemon ${props.name}`}/>
+            </div>
+            <div className={s.divcontainer}>
+                
+                <div className={s.divtipos}> 
+                    {props.tipos.map ((e, i) => {
+                        return <span key={i} className={`${s[e.name]} ${s}.tipos`}>{e.name}</span>
+                    }
+                    )}
                 </div>
-                <div className={s.divcontainer}>
-                    
-                    <div className={s.divtipos}> 
-                        {props.tipos.map ((e, i) => {
-                            return <span key={i} className={`${s[e.name]} ${s}.tipos`}>{e.name}</span>
-                        }
-                        )}
-                    </div>
-                </div>
-            </Link>
+            </div>
         </div>
         )
 }
